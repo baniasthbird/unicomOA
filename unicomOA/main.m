cf271a67a7b9923abe8af9dd190c76026edd7b47
baniasthbird <2002-sunshine@163.com>
2016-02-16 10:26:38 +0800

    aaa

---
+++ unicomOA/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  unicomOA
+//
+//  Created by zr-mac on 16/2/14.
+//  Copyright © 2016年 zr-mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
