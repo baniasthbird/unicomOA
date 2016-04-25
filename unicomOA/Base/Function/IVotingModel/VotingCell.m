b01af79b1611dcd3524b671035cf5877b9ee40ed
baniasthbird <2002-sunshine@163.com>
2016-04-25 17:22:41 +0800

    0425 完成公告界面修改，正在修改投票界面

--- unicomOA/Base/Function/IVotingModel/VotingCell.m
+++ unicomOA/Base/Function/IVotingModel/VotingCell.m
@@ -22,18 +22,18 @@
 
 @implementation VotingCell
 
-@synthesize lbl_time,lbl_Titile,lbl_condition,lbl_Department;
+@synthesize lbl_time,lbl_Titile,lbl_Department;
 
-+(instancetype)cellWithTable:(UITableView *)tableView withCellHeight:(CGFloat)cellHeight titleX:(CGFloat)i_TitleX titleY:(CGFloat)i_TitleY titleW:(CGFloat)i_TitleW titleH:(CGFloat)i_TitleH ConditionX:(CGFloat)i_ConditionX CondiditonY:(CGFloat)i_ConditionY ConditionW:(CGFloat)i_ConditionW ConditionH:(CGFloat)i_ConditionH DepartX:(CGFloat)i_DepartX DepartY:(CGFloat)i_DepartY DepartW:(CGFloat)i_DepartW DepartH:(CGFloat)i_DepartH TimeX:(CGFloat)i_TimeX TimeY:(CGFloat)i_TimeY TimeW:(CGFloat)i_TimeW TimeH:(CGFloat)i_TimeH {
++(instancetype)cellWithTable:(UITableView *)tableView withCellHeight:(CGFloat)cellHeight titleX:(CGFloat)i_TitleX titleY:(CGFloat)i_TitleY titleW:(CGFloat)i_TitleW titleH:(CGFloat)i_TitleH DepartX:(CGFloat)i_DepartX DepartY:(CGFloat)i_DepartY DepartW:(CGFloat)i_DepartW DepartH:(CGFloat)i_DepartH TimeX:(CGFloat)i_TimeX TimeY:(CGFloat)i_TimeY TimeW:(CGFloat)i_TimeW TimeH:(CGFloat)i_TimeH atIndexPath:(NSIndexPath *)indexPath {
     static NSString *cellID=@"sideslipCell";
-    VotingCell *cell=[tableView dequeueReusableCellWithIdentifier:cellID];
+    VotingCell *cell=[tableView cellForRowAtIndexPath:indexPath];
     if (!cell) {
-        cell=[[VotingCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellID withCellHeight:cellHeight titleX:i_TitleX titleY:i_TitleY titleW:i_TitleW titleH:i_TitleH condition_X:i_ConditionX condition_Y:i_ConditionY condition_W:i_ConditionW condition_H:i_ConditionH depart_X:i_DepartX depart_Y:i_DepartY depart_W:i_DepartW depart_H:i_DepartH time_X:i_TimeX time_Y:i_TimeY time_W:i_TimeW time_H:i_TimeH];
+        cell=[[VotingCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellID withCellHeight:cellHeight titleX:i_TitleX titleY:i_TitleY titleW:i_TitleW titleH:i_TitleH depart_X:i_DepartX depart_Y:i_DepartY depart_W:i_DepartW depart_H:i_DepartH time_X:i_TimeX time_Y:i_TimeY time_W:i_TimeW time_H:i_TimeH];
     }
     return cell;
 }
 
--(id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier withCellHeight:(CGFloat)cellHeight titleX:(CGFloat)i_titleX titleY:(CGFloat)i_titleY titleW:(CGFloat)i_titleW titleH:(CGFloat)i_titleH condition_X:(CGFloat)i_conditionX condition_Y:(CGFloat)i_conditionY condition_W:(CGFloat)i_conditionW condition_H:(CGFloat)i_conditionH depart_X:(CGFloat)i_departX depart_Y:(CGFloat)i_departY depart_W:(CGFloat)i_departW depart_H:(CGFloat)i_departH time_X:(CGFloat)i_timeX time_Y:(CGFloat)i_timeY time_W:(CGFloat)i_timeW time_H:(CGFloat)i_timeH {
+-(id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier withCellHeight:(CGFloat)cellHeight titleX:(CGFloat)i_titleX titleY:(CGFloat)i_titleY titleW:(CGFloat)i_titleW titleH:(CGFloat)i_titleH depart_X:(CGFloat)i_departX depart_Y:(CGFloat)i_departY depart_W:(CGFloat)i_departW depart_H:(CGFloat)i_departH time_X:(CGFloat)i_timeX time_Y:(CGFloat)i_timeY time_W:(CGFloat)i_timeW time_H:(CGFloat)i_timeH {
     if (self=[super initWithStyle:style reuseIdentifier:reuseIdentifier]) {
         _cellHeight=cellHeight;
 #pragma mark 创建滚动条，增加删除按钮
@@ -72,16 +72,14 @@
         UIView *baseView=[[UIView alloc]initWithFrame:scrollView.bounds];
         
         lbl_Titile=[[UILabel alloc]initWithFrame:CGRectMake(i_titleX, i_titleY, i_titleW, i_titleH)];
-        lbl_condition=[[UILabel alloc]initWithFrame:CGRectMake(i_conditionX, i_conditionY, i_conditionW, i_conditionH)];
+        _img_condition=[[UIImageView alloc]initWithFrame:CGRectMake(self.contentView.frame.origin.x, self.contentView.frame.origin.y, 47, 40)];
+        
         lbl_Department=[[UILabel alloc]initWithFrame:CGRectMake(i_departX, i_departY, i_departW, i_departH)];
         lbl_time=[[UILabel alloc]initWithFrame:CGRectMake(i_timeX, i_timeY, i_timeW, i_timeH)];
         lbl_Titile.font=[UIFont systemFontOfSize:18];
         lbl_Titile.textColor=[UIColor blackColor];
         lbl_Titile.numberOfLines=0;
         
-        lbl_condition.font=[UIFont systemFontOfSize:16];
-        lbl_condition.textColor=[UIColor colorWithRed:33/255.0f green:155/255.0f blue:213/255.0f alpha:1];
-        lbl_condition.numberOfLines=0;
         
         lbl_Department.font=[UIFont systemFontOfSize:14];
         lbl_Department.textColor=[UIColor lightGrayColor];
@@ -90,7 +88,7 @@
         lbl_time.textColor=[UIColor lightGrayColor];
         
         [baseView addSubview:lbl_Titile];
-        [baseView addSubview:lbl_condition];
+        [baseView addSubview:_img_condition];
         [baseView addSubview:lbl_Department];
         [baseView addSubview:lbl_time];
         _baseView=baseView;
